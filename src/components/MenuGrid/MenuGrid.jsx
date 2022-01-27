5ae15f82c67171008f7738f366ab9da2e1641bba
Murtaza Hatim <dev@murtaza.info>
2022-01-27 11:34:27 +0800

    commit random

--- src/components/MenuGrid/MenuGrid.jsx
+++ src/components/MenuGrid/MenuGrid.jsx
@@ -26,6 +26,10 @@ const MenuGrid = ({ category }) => {
         <MealCard name="Fried Potato" inventory="20" price="15" image="https://www.pixelstalk.net/wp-content/uploads/2016/08/Desktop-Food-HD-Photos.jpg"/>
         <MealCard name="Fried Potato" inventory="20" price="15" image="https://www.pixelstalk.net/wp-content/uploads/2016/08/Desktop-Food-HD-Photos.jpg"/>
         <MealCard name="Fried Potato" inventory="20" price="15" image="https://www.pixelstalk.net/wp-content/uploads/2016/08/Desktop-Food-HD-Photos.jpg"/>
+        {meals.map((meal, idx) => (
+          <MealCard id={idx} name={meal.name} inventory={meal.inventory} price={meal.price} image={meal.image} />
+        ))}
+      
       </div>
     </div>
   );
